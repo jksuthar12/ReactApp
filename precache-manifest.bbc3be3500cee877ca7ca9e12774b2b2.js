2ae5027626c2233b7b10df39bd914143f49263d7
jksuthar <jksuthar0225@gmail.com>
2020-10-13 10:13:40 +0530

    Updates

---
+++ precache-manifest.bbc3be3500cee877ca7ca9e12774b2b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d5cf030e505edd76a4af4729216e10d",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "6d92df31e7bfbba199c9",
+    "url": "/ReactApp/static/css/main.4138ea09.chunk.css"
+  },
+  {
+    "revision": "f898a8bf74d08ad741ba",
+    "url": "/ReactApp/static/js/2.49e03a0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactApp/static/js/2.49e03a0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d92df31e7bfbba199c9",
+    "url": "/ReactApp/static/js/main.b472dc7b.chunk.js"
+  },
+  {
+    "revision": "029ea820835dacbd0148",
+    "url": "/ReactApp/static/js/runtime-main.7ce5ac92.js"
+  }
+]);
